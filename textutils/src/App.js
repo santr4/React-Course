8f2a5ce7cccb87b04eb74c8356c7315363d7d596
santr4 <santra.aaranyak16092003@gmail.com>
2023-10-16 18:02:23 +0530

    JSX introduction

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
+
+let name = "aaranyak santra";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          I am right now learning Reactjs.
-        </a>
-      </header>
-    </div>
+    <>
+      <nav>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </nav>
+      <div className="container">
+        <p>hello everyone! my name is {name}.</p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius exercitationem inventore odio, veritatis beatae assumenda necessitatibus qui cumque voluptatum, corrupti alias quis nemo nobis autem neque expedita. Enim incidunt asperiores temporibus molestias suscipit vel.
+      </div>
+    </>
   );
 }
 
