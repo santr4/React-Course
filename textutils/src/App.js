1ecf8312032893ed5a5be3648fbb72ae3bcb3357
santr4 <santra.aaranyak16092003@gmail.com>
2023-10-16 18:50:16 +0530

    adding bootstrap to the react application

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -2,19 +2,43 @@
 import './App.css';
 
 
-let name = "aaranyak santra";
+
 function App() {
   return (
     <>
-      <nav>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/">TextUtils</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/">About</a>
+              </li>
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Links
+                </a>
+                <ul className="dropdown-menu">
+                  <li><a className="dropdown-item" href="/">Link1</a></li>
+                  <li><a className="dropdown-item" href="/">Link2</a></li>
+                  <li><hr className="dropdown-divider" /></li>
+                  <li><a className="dropdown-item" href="/">Link3</a></li>
+                </ul>
+              </li>
+            </ul>
+            <form className="d-flex" role="search">
+              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+              <button className="btn btn-outline-success" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
       </nav>
-      <div className="container">
-        <p>hello everyone! my name is {name}.</p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius exercitationem inventore odio, veritatis beatae assumenda necessitatibus qui cumque voluptatum, corrupti alias quis nemo nobis autem neque expedita. Enim incidunt asperiores temporibus molestias suscipit vel.
-      </div>
     </>
   );
 }
