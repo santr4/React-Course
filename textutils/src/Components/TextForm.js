1c7716cc9dcb616a1dd060ae3fec2ab3e32b6977
santr4 <santra.aaranyak16092003@gmail.com>
2023-10-17 12:13:13 +0530

    button added

--- textutils/src/Components/TextForm.js
+++ textutils/src/Components/TextForm.js
@@ -4,10 +4,14 @@ import PropTypes from 'prop-types';
 export default function TextForm(props) {
     return (
         <div className="mb-3">
-            <label htmlFor="myBox" className="htmlForm-label"><b>{props.text}</b></label>
+            <label htmlFor="myBox" className="htmlForm-label">
+                <b>{props.text}</b>
+                <p>Your text will get analyzed here.</p>
+            </label>
             <br/>
             <br/>
             <textarea class="htmlForm-control" id="myBox" rows={props.row + 5} cols={150}></textarea>
+            <button className="btn btn-primary">Convert to UpperCase</button>
         </div>
     )
 }
