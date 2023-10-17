5bde55150652d8eb13b20c75fe0a7cea115669fa
santr4 <santra.aaranyak16092003@gmail.com>
2023-10-17 19:32:45 +0530

    event handler and states

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,5 +1,6 @@
 // import logo from './logo.svg';
-// After compilation, JSX expressions become regular JavaScript function calls and evaluate to JavaScript objects
+// After compilation, JSX expressions become regular JavaScript function calls and evaluate to JavaScript objects.
+// Js is a case sensitive language.
 
 /*
 In React, "props" is short for "properties," and it refers to a mechanism for passing data from a parent component to a child component. Props are a fundamental concept in React and are used to make components more dynamic and reusable.
@@ -8,38 +9,17 @@ In React, "props" is short for "properties," and it refers to a mechanism for pa
 import './App.css';
 import Navbar from './Components/Navbar';
 import TextForm from './Components/TextForm';
-import User from './Other/User';
 
 
-const Name = "aaranyak santra";
 
 
 function App() {
-  function formatName(user){
-    return user.firstName + ' ' + user.lastName;
-  }
-  
-  const user = {
-    firstName: 'Aaranyak',
-    lastName: 'Santra'
-  }
   return (
     <>
       <Navbar title="TextUtils" aboutText="About Us" />
       <br />
       <div className="container">
         <TextForm row={5} text="Enter your text here" />
-      </div>
-      <div className="hello_name">
-        <h1>Hello! {Name}</h1>;
-      </div>
-      <div className="format_name">
-          <h1>
-            hello, {formatName(user)}
-          </h1>
-      </div>
-      <div>
-        <User user={user}/>
       </div>
     </>
   );
--- textutils/src/Components/TextForm.js
+++ textutils/src/Components/TextForm.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
 import PropTypes from 'prop-types';
 
 export default function TextForm(props) {
+    const [text, setText] = useState('Enter your text here.'); // react hooks
+    const handleUpClick = ()=>{
+        console.log("Uppercase button was clicked" + text);
+        let newText = text.toUpperCase();
+        setText(newText);
+    }
+
+    const handleOnChange = (event)=>{
+        console.log('On Change');
+        setText(event.target.value);
+    }
+
     return (
         <div className="mb-3">
             <label htmlFor="myBox" className="htmlForm-label">
@@ -10,8 +22,8 @@ export default function TextForm(props) {
             </label>
             <br/>
             <br/>
-            <textarea class="htmlForm-control" id="myBox" rows={props.row + 5} cols={150}></textarea>
-            <button className="btn btn-primary">Convert to UpperCase</button>
+            <textarea className="htmlForm-control" value = {text} id="myBox" rows={props.row + 5} onChange={handleOnChange} cols={150}></textarea>
+            <button className="btn btn-primary" onClick={handleUpClick}>Convert to UpperCase</button>
         </div>
     )
 }
