7de545c93c97160cc892ea90aed08faa52726b0c
santr4 <santra.aaranyak16092003@gmail.com>
2023-10-17 20:18:59 +0530

    number of words, characters, min read, lowercase and dark navbar added

--- textutils/src/Components/TextForm.js
+++ textutils/src/Components/TextForm.js
@@ -1,48 +1,53 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types';
 
 export default function TextForm(props) {
-    const [text, setText] = useState('Enter your text here.'); // react hooks
-    const handleUpClick = ()=>{
+    const [text, setText] = useState(''); // react hooks
+    const handleUpClick = () => {
         // console.log("Uppercase button was clicked" + text);
         let newText = text.toUpperCase();
         setText(newText);
     }
 
-    const handleLowerClick = ()=>{
+    const handleLowerClick = () => {
         let newText = text.toLowerCase();
         setText(newText);
     }
 
-    const handleOnChange = (event)=>{
+    const handleOnChange = (event) => {
         setText(event.target.value);
     }
 
     return (
         <> {/* this is fragment in react*/}
-        <div className="mb-3">
-            <label htmlFor="myBox" className="htmlForm-label">
-                <b>{props.text}</b>
-                <p>Your text will get analyzed here.</p>
-            </label>
-            <br/>
-            <br/>
-            <textarea className="htmlForm-control" value = {text} id="myBox" rows={props.row + 5} onChange={handleOnChange} cols={150}></textarea>
-            <button className="btn btn-primary" onClick={handleUpClick}>UpperCase</button>
-            <button className="btn btn-primary my-3 mx-2" onClick={handleLowerClick}>LowerCase</button>
-        </div>
-        <div className="container1 my-5">
-            <h1>Your text summary</h1>
-            <p>{text.split(" ").length} words and {text.length} characters. </p>
-        </div>
+            <div className="mb-3">
+                <label htmlFor="myBox" className="htmlForm-label">
+                    <b>{props.text}</b>
+                    <p style={{fontsize : '12px'}}>Your text will get analyzed here.</p>
+                </label>
+                <br />
+                <br />
+                <textarea className="htmlForm-control" value={text} id="myBox" rows={props.row + 5} onChange={handleOnChange} cols={150}></textarea>
+                <button className="btn btn-primary" onClick={handleUpClick}>UpperCase</button>
+                <button className="btn btn-primary my-3 mx-2" onClick={handleLowerClick}>LowerCase</button>
+            </div>
+            <div className="container1 my-5">
+                <h2>Your text summary</h2>
+                <p>{text.split(" ").length} words and {text.length} characters. </p>
+                <p>{0.008 * text.split(" ").length} Minutes Read. </p>
+            </div>
+            <div className="container2 my-3">
+                <h2>Preview</h2>
+                <p>{text}</p>
+            </div>
         </>
     )
 }
 
-TextForm.propTypes={
-    row : PropTypes.number
+TextForm.propTypes = {
+    row: PropTypes.number
 }
 
-TextForm.defaultProps={
-    row : 10
+TextForm.defaultProps = {
+    row: 10
 }
