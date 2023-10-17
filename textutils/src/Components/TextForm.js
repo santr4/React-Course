fcf587f0496f540bb33127dd6c7742d587021aa7
santr4 <santra.aaranyak16092003@gmail.com>
2023-10-17 19:59:15 +0530

    another function to change the state

--- textutils/src/Components/TextForm.js
+++ textutils/src/Components/TextForm.js
@@ -9,11 +9,17 @@ export default function TextForm(props) {
         setText(newText);
     }
 
+    const handleLowerClick = ()=>{
+        let newText = text.toLowerCase();
+        setText(newText);
+    }
+
     const handleOnChange = (event)=>{
         setText(event.target.value);
     }
 
     return (
+        <> {/* this is fragment in react*/}
         <div className="mb-3">
             <label htmlFor="myBox" className="htmlForm-label">
                 <b>{props.text}</b>
@@ -22,8 +28,14 @@ export default function TextForm(props) {
             <br/>
             <br/>
             <textarea className="htmlForm-control" value = {text} id="myBox" rows={props.row + 5} onChange={handleOnChange} cols={150}></textarea>
-            <button className="btn btn-primary" onClick={handleUpClick}>Convert to UpperCase</button>
+            <button className="btn btn-primary" onClick={handleUpClick}>UpperCase</button>
+            <button className="btn btn-primary my-3 mx-2" onClick={handleLowerClick}>LowerCase</button>
         </div>
+        <div className="container1 my-5">
+            <h1>Your text summary</h1>
+            <p>{text.split(" ").length} words and {text.length} characters. </p>
+        </div>
+        </>
     )
 }
 
